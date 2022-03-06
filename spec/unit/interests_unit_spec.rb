171904a3bac8186ae171448f4a47d65eec42aee0
Garrett-Freddo <gafreddo@gmail.com>
2022-03-06 17:17:55 -0600

    fixed interests test cases

--- spec/unit/interests_unit_spec.rb
+++ spec/unit/interests_unit_spec.rb
@@ -3,7 +3,11 @@ require 'rails_helper'
 
 RSpec.describe Interest, type: :model do
   subject do
-    described_class.new(interestsId: 12345678, userId: 12345678, careerInterest: "sports", hobbies: "football", favMovieGenres: "sports", numberPrereqs: 1, expectations: "none" )
+    described_class.new(userId: 12345678, phone: "12345678",
+    nationality: "Native American", state: "Texas", community: "Rise",
+    numPrereqs: 1, scholarship: "none", faveMovieGenre: "Action", potentialRoles:"leader",
+    numWorkExp: 1, usedTech: "Rails", projects: "Canadian wombat photography",
+    extracurric: "Photography", pastWorkExp: "Amazon")
 
   end
 
@@ -11,32 +15,60 @@ RSpec.describe Interest, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'is not valid without a id' do
-    subject.interestsId = nil
-    expect(subject).not_to be_valid
-  end
-  it 'is not valid without any interests' do
-    subject.careerInterest = nil
-    expect(subject).not_to be_valid
-  end
   it 'is not valid without a user id' do
     subject.userId = nil
     expect(subject).not_to be_valid
   end
-  it 'is not valid without hobbies' do
-    subject.hobbies = nil
+  it 'is not valid without a phone #' do
+    subject.phone = nil
     expect(subject).not_to be_valid
   end
-  it 'is not valid without a movie genres' do
-    subject.favMovieGenres = nil
+  it 'is not valid without a nationality' do
+    subject.nationality = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without state' do
+    subject.state = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without a community' do
+    subject.community = nil
     expect(subject).not_to be_valid
   end
   it 'is not valid without prereqs' do
-    subject.numberPrereqs = nil
+    subject.numPrereqs = nil
     expect(subject).not_to be_valid
   end
-  it 'is not valid without expectations' do
-    subject.expectations = nil
+  it 'is not valid without scholarship' do
+    subject.scholarship = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without movie genre' do
+    subject.faveMovieGenre = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without potential roles' do
+    subject.potentialRoles = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without work experience amount' do
+    subject.numWorkExp = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without used tech' do
+    subject.usedTech = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without projects' do
+    subject.projects = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without extracurriculars' do
+    subject.extracurric = nil
+    expect(subject).not_to be_valid
+  end
+  it 'is not valid without past work experience' do
+    subject.pastWorkExp = nil
     expect(subject).not_to be_valid
   end
 end
