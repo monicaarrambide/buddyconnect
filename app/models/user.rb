24119c4b6c339d3d9c8733144be5ab66fbd78123
Harrison Platt <harrison7718@gmail.com>
2022-03-27 21:57:59 -0500

    creates random student Id

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,21 +13,16 @@ class User < ApplicationRecord
     def self.from_google(email:, full_name:, uid:, avatar_url:)
         return nil unless /@tamu.edu\z/.match?(email)
         # create_with(uid: uid, full_name: full_name, avatar_url: avatar_url).find_or_create_by!(tamuEmail: email)
-        if(User.where(tamuEmail: email).length() > 0)
-            find_or_create_by!(tamuEmail:email)
-        else
-            # redirect_to new_user_path
-            create_with(
-              studentId: -1,
-              firstName: "firstname", 
-              lastName: "lastname", 
-              isOfficer: false,
-              isAdmin: false,
-              tamuEmail: email,
-              dateOfBirth: '2022-3-4',
-              gradAssistance: false
-            ).find_or_create_by!(tamuEmail: email)
-        end
+        create_with(
+            studentId: SecureRandom.uuid,
+            firstName: full_name, 
+            lastName: avatar_url, 
+            isOfficer: false,
+            isAdmin: false,
+            tamuEmail: email,
+            dateOfBirth: '2022-3-4',
+            gradAssistance: false
+        ).find_or_create_by!(tamuEmail: email)
         #@TODO could be a bug
-      end
+    end
 end
