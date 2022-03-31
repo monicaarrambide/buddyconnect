e1c297eecae72a004fbc62d36b3bd1b522fb0332
Harrison Platt <harrison7718@gmail.com>
2022-03-31 14:57:47 -0500

    fixed user tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_25_194319) do
+ActiveRecord::Schema.define(version: 2022_03_05_223851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
